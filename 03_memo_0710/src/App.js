2c831802a179c15a0a8f266320598327c9658a7d
future-yang <108065733+future-yang@users.noreply.github.com>
2022-07-16 01:23:49 +0900

    Add files via upload
    node_module folder excluded

---
+++ 03_memo_0710/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Todo from './components/todo';
+
+function App() {
+  return (
+    <div className='container border border-primary rounded m-5 p-5'>
+      <Todo/>
+
+    </div>
+  );
+}
+
+export default App;
